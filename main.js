f1120cd706452a646b4f1a863cc462309d7dfb64
muhaiminmdfatih <82874236+muhaiminmdfatih@users.noreply.github.com>
2021-12-26 14:51:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+objects=[];
+status="";
+function setup(){
+canvas=createCanvas(480,380);
+canvas.center()
+video=createCapture(VIDEO)
+video.size(480,380)
+video.hide()
+}
+function draw(){
+image(video,0,0,480,380);
+}
+function start(){
+objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML="Status: Detecting objects";
+object_name=document.getElementById("object_name").value
+}
+function modelLoaded(){
+console.log("Model is loaded");
+status=true;
+}
