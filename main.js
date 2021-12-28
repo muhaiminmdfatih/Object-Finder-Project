c2eea3acd22bd2ae71b753d04fd0fc0f94c7091c
muhaiminmdfatih <82874236+muhaiminmdfatih@users.noreply.github.com>
2021-12-28 17:55:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,25 @@ video.hide()
 }
 function draw(){
 image(video,0,0,480,380);
+if(status!=""){
+objectDetector.detect(video,gotResult);
+for(i=0; i<objects.length; i++){
+fill("red");
+percent=floor(objects[i].confidence*100);
+text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+noFill()
+stroke("red")
+rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+if(objects[i].label==object_name){
+video.stop();
+objectDetector.detect(gotResult);
+document.getElementById("status").innerHTML=object_name+" is found"
+}
+else{
+document.getElementById("status").innerHTML=object_name+" is not found"
+}
+}
+}
 }
 function start(){
 objectDetector=ml5.objectDetector('cocossd',modelLoaded);
@@ -18,4 +37,11 @@ object_name=document.getElementById("object_name").value
 function modelLoaded(){
 console.log("Model is loaded");
 status=true;
+}
+function gotResult(error,results){
+if(error){
+console.log(error)
+}
+console.log(results)
+objects=results
 }
